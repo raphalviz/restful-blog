02028d416a494e679863e9127d835f56641ed879
zivlA <raphviz@gmail.com>
2016-12-24 16:38:26 -0500

    sanitize

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var bodyParser      = require('body-parser'),
-    methodOverride  = require('method-override'),
-    mongoose        = require('mongoose'),
-    express         = require('express'),
-    app             = express();
+var bodyParser          = require('body-parser'),
+    methodOverride      = require('method-override'),
+    expressSanitizer    = require('express-sanitizer'),
+    mongoose            = require('mongoose'),
+    express             = require('express'),
+    app                 = express();
 
 // App config
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 // Mongoose/Model Config
@@ -45,6 +47,7 @@ app.get('/blogs/new', function(req, res) {
 
 // CREATE
 app.post('/blogs', function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
         if (err) {
             res.render("new");
@@ -78,6 +81,7 @@ app.get('/blogs/:id/edit', function(req, res) {
 
 // UPDATE
 app.put('/blogs/:id', function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if (err) {
             res.redirect('/blogs');
